13f5faed44599dc0b00cea15237104cbe15fecbb
WTiebebedigha22 <106900112+WTiebebedigha22@users.noreply.github.com>
2023-11-17 11:02:48 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,7 +27,7 @@
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Nov 17, 2023 10:00:00').getTime(),
+let countDown = new Date('Nov 18, 2023 18:30:00').getTime(),
 x = setInterval(function() {    
 
   let now = new Date().getTime(),
